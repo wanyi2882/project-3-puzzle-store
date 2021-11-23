31b7361ca47f3fa8eedc3daae0c28a1cde45f386
wanyi2882 <wanyi.2882@gmail.com>
2021-11-23 08:48:34 +0000

    Refactor to DAL for functions in listings

---
+++ dal/listings.js
@@ -0,0 +1,38 @@
+const {  Puzzle, Theme, Size, AgeGroup, DifficultyLevel, Material, Tag, Frame  } = require('../models')
+
+// Get all Themes
+async function getThemes(){
+    return await Theme.fetchAll().map(theme => [theme.get('id'), theme.get('name')])
+}
+
+// Get all Sizes
+async function getSizes(){
+    return await Size.fetchAll().map(size => [size.get('id'), size.get('pieces')])
+}
+
+// Get All Age Groups
+async function getAgeGroups(){
+    return AgeGroup.fetchAll().map(age => [age.get('id'), age.get('name')])
+}
+
+// Get All Difficulty Levels
+async function getDifficultyLevels(){
+    return DifficultyLevel.fetchAll().map(level => [level.get('id'), level.get('name')])
+}
+
+// Get All Materials
+async function getMaterials(){
+    return Material.fetchAll().map(material =>[material.get('id'), material.get('type')])
+}
+
+// Get All Tags
+async function getTags(){
+    return Tag.fetchAll().map(tag => [tag.get('id'), tag.get('name')]);
+}
+
+// Get All Frames
+async function getFrames(){
+    return Frame.fetchAll().map(frame => [frame.get('id'), frame.get('material')])
+}
+
+module.exports = { getThemes, getSizes, getAgeGroups, getDifficultyLevels, getMaterials, getTags, getFrames }
