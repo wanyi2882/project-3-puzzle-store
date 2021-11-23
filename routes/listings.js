7701ce84c6bf9d23ea7992b3965c2e4866672e3b
wanyi2882 <wanyi.2882@gmail.com>
2021-11-23 08:51:38 +0000

    Form submission post + data displayed successfully

--- routes/listings.js
+++ routes/listings.js
@@ -1,35 +1,85 @@
 const express = require("express");
 const router = express.Router();
 
-// #1 import in the Puzzle model
-const {  Puzzle, Theme, Size, AgeGroup, DifficultyLevel, Material, Tag, Frame  } = require('../models')
+// Import in the Puzzle model
+const { Puzzle, Theme, Size, AgeGroup, DifficultyLevel, Material, Tag, Frame } = require('../models')
+
+// Import in DAL
+const { getThemes, getSizes, getAgeGroups, getDifficultyLevels, getMaterials, getTags, getFrames } = require('../dal/listings')
 
 // Import in the Forms
 const { bootstrapField, createPuzzleForm } = require('../forms');
 
 router.get('/', async (req, res) => {
     // #2 - fetch all the puzzles (ie, SELECT * from products)
-    let puzzles = await Puzzle.collection().fetch();
+    let puzzles = await Puzzle.collection().fetch({
+        withRelated:['Theme', 'Size', 'AgeGroup', 'DifficultyLevel', 'Material', 'Tag', 'Frame']
+    });
     res.render('listings/index', {
         'listings': puzzles.toJSON() // #3 - convert collection to JSON
     })
 })
 
+// Displaying Create Form
 router.get('/create', async (req, res) => {
-    const themes = await Theme.fetchAll().map(theme => [theme.get('id'), theme.get('name')])
-    const sizes = await Size.fetchAll().map(size => [size.get('id'), size.get('pieces')])
-    const age_groups = await AgeGroup.fetchAll().map(age => [age.get('id'), age.get('name')])
-    const difficulty_levels = await DifficultyLevel.fetchAll().map(level => [level.get('id'), level.get('name')])
-    const materials = await Material.fetchAll().map(material =>[material.get('id'), material.get('type')])
-    const tags = await Tag.fetchAll().map(tag => [tag.get('id'), tag.get('name')]);
-    const frames = await Frame.fetchAll().map(frame => [frame.get('id'), frame.get('material')])
+    const themes = await getThemes()
+    const sizes = await getSizes()
+    const age_groups = await getAgeGroups()
+    const difficulty_levels = await getDifficultyLevels()
+    const materials = await getMaterials()
+    const tags = await getTags()
+    const frames = await getFrames()
 
     const puzzleForm = createPuzzleForm(themes, sizes, age_groups, difficulty_levels, materials, tags, frames);
-    res.render('listings/create',{
+
+    res.render('listings/create', {
         'form': puzzleForm.toHTML(bootstrapField),
         'cloudinaryName': process.env.CLOUDINARY_NAME,
         'cloudinaryApiKey': process.env.CLOUDINARY_API_KEY,
         'cloudinaryPreset': process.env.CLOUDINARY_UPLOAD_PRESET
+    })
+})
+
+// Process Submitted form
+router.post('/create', async (req, res) => {
+    const themes = await getThemes()
+    const sizes = await getSizes()
+    const age_groups = await getAgeGroups()
+    const difficulty_levels = await getDifficultyLevels()
+    const materials = await getMaterials()
+    const tags = await getTags()
+    const frames = await getFrames()
+
+    const puzzleForm = createPuzzleForm(themes, sizes, age_groups, difficulty_levels, materials, tags, frames);
+
+    puzzleForm.handle(req, {
+        'success': async (form) => {
+            // Seperate out tags and frames
+            let { tags, frames, ...puzzleData } = form.data
+
+            // Save data into a new puzzle instance
+            const puzzle = new Puzzle(puzzleData)
+            await puzzle.save()
+
+            // Save many to many relationships
+            if (tags) {
+                await puzzle.Tag().attach(tags.split(","));
+            }
+
+            if (frames) {
+                await puzzle.Frame().attach(frames.split(","));
+            }
+
+            res.redirect('/listings');
+        },
+        'error': async (form) => {
+            res.render('listings/create', {
+                'form': form.toHTML(bootstrapField),
+                'cloudinaryName': process.env.CLOUDINARY_NAME,
+                'cloudinaryApiKey': process.env.CLOUDINARY_API_KEY,
+                'cloudinaryPreset': process.env.CLOUDINARY_UPLOAD_PRESET
+            })
+        }
     })
 })
 
