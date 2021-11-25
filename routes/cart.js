2fd3005d900c40297fad02a3afbbc28d75154683
wanyi2882 <wanyi.2882@gmail.com>
2021-11-25 12:38:14 +0000

    Remove item from cart route and button in hbs

--- routes/cart.js
+++ routes/cart.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const cartServices = require('../services/cart');
 
 // Display all items in the cart
-router.get('/', async function(req,res){
+router.get('/', async function (req, res) {
     let userId = req.session.user.id;
     let cartDetails = await cartServices.getShoppingCart(userId);
     res.render('cart/index', {
@@ -14,26 +14,26 @@ router.get('/', async function(req,res){
 })
 
 // Add item to cart
-router.get('/:puzzle_id/add', async function(req,res){
+router.get('/:puzzle_id/add', async function (req, res) {
     let userId = req.session.user.id;
     let puzzleId = req.params.puzzle_id;
 
-    let status  = await cartServices.addItemToCart(userId, puzzleId);
+    let status = await cartServices.addItemToCart(userId, puzzleId);
     if (status) {
         req.flash("success_messages", "Item successfully added to cart");
         res.redirect('/listings');
     } else {
         req.flash("error_messages", "Failed to add item to shopping cart");
         res.redirect('/listings');
-    }  
+    }
 })
 
 // Process update quantity in cart
-router.post('/:puzzle_id/quantity/update', async function(req,res){
+router.post('/:puzzle_id/quantity/update', async function (req, res) {
     let status = await cartServices.updateQuantityInCart(req.session.user.id,
-                                            req.params.puzzle_id,
-                                            req.body.newQuantity
-                                            );
+        req.params.puzzle_id,
+        req.body.newQuantity
+    );
     if (status) {
         req.flash("success_messages", "Product quantity has been updated");
     } else {
@@ -41,6 +41,19 @@ router.post('/:puzzle_id/quantity/update', async function(req,res){
     }
     res.redirect('/cart');
 });
+
+// Remove item from cart
+router.post('/:puzzle_id/remove', async function (req, res) {
+    let status = await cartServices.removeFromCart(req.session.user.id, req.params.puzzle_id)
+
+    if (status) {
+        req.flash("success_messages", "Item has been successfully removed");
+    } else {
+        req.flash("error_messages", "Failed to delete item");
+    }
+
+    res.redirect('/cart');
+})
 
 
 module.exports = router;
