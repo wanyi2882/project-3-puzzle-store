4b53d0482267c641ff074ad22aa7666d98f45f5c
wanyi2882 <wanyi.2882@gmail.com>
2021-12-06 10:31:37 +0000

    Added authenication to order routes

--- routes/order.js
+++ routes/order.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+// Import Middleware
+const { checkIfAuthenticated } = require('../middlewares');
+
 const { Order, OrderStatus } = require("../models")
 
 const orderDataLayer = require("../dal/order")
 
 // Display all orders
-router.get('/', async function (req, res) {
+router.get('/', [checkIfAuthenticated], async function (req, res) {
     let allOrders = await orderDataLayer.adminGetOrder();
     res.render('orders/index', {
         'allOrders': allOrders.toJSON()
@@ -14,7 +17,7 @@ router.get('/', async function (req, res) {
 })
 
 //Display order details of each order
-router.get('/:order_id/details', async function (req, res) {
+router.get('/:order_id/details', [checkIfAuthenticated], async function (req, res) {
     let orderDetail = await orderDataLayer.adminGetOrderDetails(req.params.order_id);
 
     res.render('orders/details', {
